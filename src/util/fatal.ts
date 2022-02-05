1266b3c9f24fc860afb3e4402dd42b38325c33aa
AsPulse <aspulse.x@gmail.com>
2022-02-05 22:04:06 +0900

    Add: error relative

--- src/util/fatal.ts
+++ src/util/fatal.ts
@@ -1,4 +1,7 @@
+import { relative } from "path";
+import { cwd } from "process";
+
 export function fatal(file: string, line: number, error: string) {
-  console.error(`${file}:${line} error: ${error}`);
+  console.error(`${relative(cwd(), file)}:${line} error: ${error}`);
   process.exit(1);
 }
