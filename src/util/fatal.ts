36deb2cb494af073642d1b3966248eb9108eb419
AsPulse <aspulse.x@gmail.com>
2022-02-05 22:10:38 +0900

    Add: path resolver

--- src/util/fatal.ts
+++ src/util/fatal.ts
@@ -1,7 +1,8 @@
-import { relative } from "path";
+import { relative, resolve } from "path";
 import { cwd } from "process";
 
 export function fatal(file: string, line: number, error: string) {
-  console.error(`${relative(cwd(), file)}:${line} error: ${error}`);
+  const pos = './' + relative(cwd(), file).replace(/\\/g, '/');
+  console.error(`${pos}:${line} error: ${error}`);
   process.exit(1);
 }
