5a394c8318a08033199bce46360c2241fc3f073f
AsPulse <aspulse.x@gmail.com>
2022-02-05 22:24:59 +0900

    Fixmore: error style

--- src/util/fatal.ts
+++ src/util/fatal.ts
@@ -3,6 +3,6 @@ import { cwd } from "process";
 
 export function fatal(file: string, line: number, error: string) {
   console.error(file);
-  console.error(`##[error] ${line}:1 ${error}`);
+  console.error(`##[error]  ${line}:1  error  ${error}`);
   process.exit(1);
 }
