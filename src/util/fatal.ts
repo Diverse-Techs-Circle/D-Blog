5ddd03b2714e11bf40b4a95a816e38b14ba36d56
AsPulse <aspulse.x@gmail.com>
2022-02-05 22:30:37 +0900

    Add: re-in space

--- src/util/fatal.ts
+++ src/util/fatal.ts
@@ -2,7 +2,9 @@ import { relative, resolve } from "path";
 import { cwd } from "process";
 
 export function fatal(file: string, line: number, error: string) {
+  console.log();
   console.error(file);
   console.error(`##[error]  ${line}:1  error  ${error}`);
+  console.log();
   process.exit(1);
 }
