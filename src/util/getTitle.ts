46aaf12d94e97cff83d10c4fc26f34fcb9c0b438
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-02-06 20:52:39 +0900

    Feature/linkcard (#20)
    * Add: linkcard parse
    
    * Add: card stylesheet
    
    * Add: hover effect
    
    * Add: newtab

---
+++ src/util/getTitle.ts
@@ -0,0 +1,7 @@
+import got from "got";
+
+export async function getGlobalPageTitle(url: string) {
+  const title =(await got(url)).rawBody.toString().replace(/(\n|\r)/g, '').match(/<head>(.*)<title>(.+?)<\/title>(.*)<\/head>/);
+  if (!title) return '';
+  return title[2];
+}
