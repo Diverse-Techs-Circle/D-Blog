da509c344aeeeaada4138cb29a68f8ca136567f5
AsPulse <aspulse.x@gmail.com>
2022-02-06 11:23:20 +0900

    Add: buildMeta

--- src/page/header.ts
+++ src/page/header.ts
@@ -11,4 +11,20 @@ type IMetaData =
   { name: 'referrer', content: 'no-referrer' | 'origin' | 'no-referrer-when-downgrade' | 'origin-when-crossorigin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-URL' } |
   { name: 'color-scheme', content: 'normal' | 'only light' | 'only dark' | 'light dark' | 'dark light' } |
   { name: 'googlebot' | 'robots' | 'slurp', content: ('index' | 'noindex' | 'follow' | 'nofollow' | 'none' | 'noodp' | 'noarchive' | 'nosnippet' | 'noimageindex' | 'nocache')[] } |
-  { name: 'viewport', content: ViewportContent[] }
+  { name: 'viewport', content: ViewportContent[] };
+
+export function buildMeta(meta: IMetaData): string {
+  if ( 'charset' in meta ) {
+    return `<meta charset="${meta.charset}">`;
+  }
+  if ( 'httpEquiv' in meta ) {
+    return `<meta http-equiv="${meta.httpEquiv}" content=${meta.content}>`;
+  }
+  if ( meta.name === 'googlebot' || meta.name === 'robots' || meta.name === 'slurp') {
+    return `<meta name="${meta.name}" content="${meta.content.join(', ')}">`
+  }
+  if ( meta.name === 'viewport' ) {
+    return `<meta name="${meta.name}" content="${meta.content.map(v => `${v.key}=${v.value}`).join(', ')}">`;
+  }
+  return `<meta name="${meta.name}" content="${meta.content}">`;
+}
