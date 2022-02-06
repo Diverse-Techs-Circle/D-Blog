4e34fa7eea6cbe9c845782ada3c729205090c0e4
AsPulse <aspulse.x@gmail.com>
2022-02-06 13:38:00 +0900

    Add: externals

--- buildAuthorSuggestion.js
+++ buildAuthorSuggestion.js
@@ -5,6 +5,7 @@ build({
   entryPoints: [resolve(__dirname, 'src/authorSuggestion.ts')],
   target: 'esnext',
   outdir: resolve(__dirname, 'dist'),
+  external: ['node:http', 'node:util', 'node:buffer', 'node:https', 'node:url', 'node:net', 'node:tls', 'node:process', 'node:stream', 'node:events'],
   bundle: true,
   minify: true,
   sourcemap: true,
