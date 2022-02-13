d070b0ec73cfea30a8e8813eefa4d176184edbbf
AsPulse <aspulse.x@gmail.com>
2022-02-13 22:44:29 +0900

    Merge: font/mplus

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { DBlogInstance } from './instance';
 const instance = new DBlogInstance({
     contentPath: resolve(cwd(), 'content'),
     webPath: resolve(cwd(), 'www'),
-    mplus:  resolve(cwd(), 'src', 'asset', 'font'),
+    mplus: resolve(cwd(), 'src', 'asset', 'font'),
     siteUrl: 'https://diverse-techs-circle.github.io/D-Blog/',
     domainPrefix: '/D-Blog',
     relativePath: true,
